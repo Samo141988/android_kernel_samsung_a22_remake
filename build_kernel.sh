289a2869ed3324f1d8aae12cb821f3ccb2efa597
Samo141988 <154265838+Samo141988@users.noreply.github.com>
2025-03-15 06:41:48 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,7 +9,7 @@ export ANDROID_MAJOR_VERSION=t
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y f22_defconfig
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a22_defconfig
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
